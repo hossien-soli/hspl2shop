ac38335be23f1bef4ac0a8da6a9a1d54499f5119
Hossien-Soleimani <h.soli.jff@gmail.com>
2025-12-10 14:56:40 +0330

    a fresh spring-boot 4 application

---
+++ src/main/java/dev/hspl/hspl2shop/Hspl2shopApplication.java
@@ -0,0 +1,11 @@
+package dev.hspl.hspl2shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hspl2shopApplication {
+    static void main(String[] args) {
+        SpringApplication.run(Hspl2shopApplication.class, args);
+    }
+}
---
+++ src/test/java/dev/hspl/hspl2shop/Hspl2shopApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hspl.hspl2shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hspl2shopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
